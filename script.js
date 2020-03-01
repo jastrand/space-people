f5014d845b1b8b01f349654442cee0f889a0260f
Johanna Åstrand <johanna@johannas-mbp.mynet>
2020-03-01 19:22:53 +0100

    added launch name, mission and time stamps

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const container = document.getElementById("main");
-const flightCountHeader = document.getElementById("flightCount");
+const launchCountHeader = document.getElementById("launchCount");
 const apiUrl = 'https://api.spacexdata.com/v3/launches/past';
+
 
 fetch(apiUrl)
   .then((response) => {
@@ -9,6 +10,26 @@ fetch(apiUrl)
   })
 
   .then((launchArray) => {
-    const numLaunches = launchArray.length
-    console.log(numLaunches)
+    const numLaunches = launchArray.length;
+    launchCountHeader.innerHTML += `${numLaunches}`
+
+    launchArray.forEach((launch) => {
+      const launchDate = new Date(launch.launch_date_utc);
+
+      const launchImage = `${launch.links.mission_patch_small}`
+
+      const launchDateString = launchDate.toLocaleDateString('en-US');
+      const launchTimeString = launchDate.toLocaleTimeString('en-US');
+
+
+
+      container.innerHTML += `
+      <h2>${launch.flight_number}. ${launch.mission_name}</h2>
+      <img src="${launchImage}">
+      <p>Launch date: ${launchDateString} ${launchTimeString}</p>
+      <p>Succeded: ${launch.launch_success}</p>
+      <p>Details: ${launch.details}</p>
+      `
+    })
+
   })
